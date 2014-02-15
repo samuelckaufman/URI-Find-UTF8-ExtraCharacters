57f886d12835a87b6a353640d5332749729964ca
skaufman <sam@socialflow.com>
2014-02-15 16:28:07 -0500

    URI::Find::UTF8::ExtraCharacters

---
+++ lib/URI/Find/UTF8/ExtraCharacters.pm
@@ -0,0 +1,22 @@
+package URI::Find::UTF8::ExtraCharacters;
+
+use strict;
+use warnings;
+use Data::Dumper::Concise;
+
+use base 'URI::Find::UTF8';
+our @_chars;
+sub uric_set {
+    my $self = shift;
+    join('', map { quotemeta($_) } @{ $self->{_extra_characters} } )
+        . $self->SUPER::uric_set;
+}
+
+sub new {
+    my($class,$callback,%params) = @_;
+    my $extra_characters = $params{extra_characters} || [];
+    my $self = $class->SUPER::new($callback);
+    $self->{_extra_characters} = $extra_characters;
+    return $self;
+}
+1;
